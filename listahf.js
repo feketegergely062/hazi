50852af086ec058f00e7a979691f8758c5bcefd7
feketegergely <fekete.gergely@gmail.com>
2023-04-15 20:27:26 +0200

    fel kesz se

---
+++ listahf.js
@@ -0,0 +1,32 @@
+console.log("jó e?!");
+window.addEventListener('contextmenu', (event) => {
+  event.preventDefault();
+});
+let sor = 12;
+let oszlop = 4;
+
+let lista = [
+  { id: 1, nev: "Andó Attila", cim: "2051 Biatorbágy, Petőfi utva 15.", eletkor: 24 },
+  { id: 2, nev: "Jáger Attila", cim: "1134 Bp, Váci út 47.", eletkor: 48 },
+  { id: 3, nev: "Kovács Attila", cim: "2013 Pomáz, Arany J utca 12", eletkor: 22 },
+  { id: 4, nev: "Kovács Pál", cim: "9027 Győr, Nagy Ernő utca 30", eletkor: 50 },
+  { id: 5, nev: "Dés László", cim: "2119 Pécel, Mák utca 10", eletkor: 41 },
+  { id: 6, nev: "Árva Ferenc", cim: "5000 Szolnok, Dió utca 20", eletkor: 32 },
+  { id: 7, nev: "Lakatos Pál", cim: "3700 Kazincbarcika, Barcika tér 1", eletkor: 15 },
+  { id: 8, nev: "Szomszéd Tibor", cim: "6700 Szeged, Dóm tér  10", eletkor: 22 },
+  { id: 9, nev: "Kocsis Károly", cim: "4246 Nyiregyháza, Komló köz 1", eletkor: 33 },
+  { id: 10, nev: "Kis Tamara", cim: "4000 Debrecen, Nagy Ernő utca 10", eletkor: 44 },
+  { id: 11, nev: "Szendrei Krisztián", cim: "3500 Miskolc, Nagy Ernő utca 10", eletkor: 66 },
+  { id: 12, nev: "Nagy Tibor", cim: "7600 Pécs, Kocsma utca 10", eletkor: 17 },
+];
+
+
+
+let oszlopok = [
+  emberek = [
+    { key: "id", texthu: "Azonosító", texten: "Id" },
+    { key: "nev", texthu: "Név", texten: "Name" },
+    { key: "cim", texthu: "Cím", texten: "Address" },
+    { key: "eletkor", texthu: "Életkor", texten: "Age" },
+  ]
+]
